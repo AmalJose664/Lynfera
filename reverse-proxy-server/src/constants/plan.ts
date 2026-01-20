55db565112febccd5de145e37599f84e1825c26a
Amal Jose <amal446446@gmail.com>
2026-01-21 02:18:46 +0530

    fix(reverse-proxy): plan changed

--- reverse-proxy-server/src/constants/plan.ts
+++ reverse-proxy-server/src/constants/plan.ts
@@ -1,54 +1,36 @@
-
+type EachPlanFields = {
+	name: string;
+	slug: string;
+	pricePerMonth: number;
+	maxProjects: number;
+	concurrentBuilds: number;
+	maxDailyDeployments: number;
+	totalBandwidthGB: number;
+	features?: string[];
+}
 export interface IPlans {
-	FREE: {
-		name: string;
-		slug: string;
-		pricePerMonth: number;
-		maxProjects: number;
-		maxDailyDeployments: number;
-		totalBandwidthGB: number
-		features: string[];
-	};
-	PRO: {
-		name: string;
-		slug: string;
-		pricePerMonth: number;
-		maxProjects: number;
-		maxDailyDeployments: number;
-		totalBandwidthGB: number
-		features: string[];
-	};
+	FREE: EachPlanFields;
+	PRO: EachPlanFields
 }
 export const PLANS: IPlans = {
 	FREE: {
 		name: "FREE",
 		slug: "Starter",
 		pricePerMonth: 0,
-		maxProjects: 8,
+		maxProjects: 10,
+		concurrentBuilds: 1,
 		maxDailyDeployments: 8,
 		totalBandwidthGB: 100,
-		features: [
-			"Basic hosting",
-			"Up to 8 projects",
-			"Max 8 deployments daily",
-			"100GB Total Bandwidth",
-			"No custom sub domains"
-		],
+		features: [] // view in frontend code
 	},
 	PRO: {
 		name: "PRO",
 		slug: "Pro",
 		pricePerMonth: 5,
-		maxProjects: 20,
+		maxProjects: 30,
+		concurrentBuilds: 3,
 		maxDailyDeployments: 40,
 		totalBandwidthGB: 1000,
-		features: [
-			"Priority builds",
-			"Custom sub domains",
-			"Up to 20 projects",
-			"Max 40 deployments daily",
-			"1TB Total Bandwidth",
-			"More resources"
-		],
+		features: [] // view in frontend code
 	},
 } as const;
