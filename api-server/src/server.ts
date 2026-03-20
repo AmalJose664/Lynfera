ceecfdadb7bded5932979b90b6dc333850f6f535
Amal Jose <amal446446@gmail.com>
2026-03-20 16:50:51 +0530

    feat: added logs to view usages

--- api-server/src/server.ts
+++ api-server/src/server.ts
@@ -3,10 +3,23 @@ import httpServer from "./app.js";
 import { intervalManager, redisCacheService } from "./instances.js";
 import { startKafkaConsumer, stopKafkaConsumer } from "./events/index.js";
 import { ENVS } from "@/config/env.config.js";
+import { cpus, freemem, loadavg, totalmem } from "os";
 const PORT = ENVS.PORT || 8000;
 
 const startServer = async () => {
 	console.log("starting.....");
+	if (ENVS.NODE_ENV === "production") {
+		const totalMem = totalmem() / (1024 ** 3);
+		const freeMem = freemem() / (1024 ** 3);
+		const cpuCores = cpus().length;
+		const load = loadavg(); // [1m, 5m, 15m]
+
+		console.log(`Server started `);
+		console.log(`CPU cores: ${cpuCores}`);
+		console.log(`Load avg: ${load}`);
+		console.log(`RAM total: ${totalMem.toFixed(2)} GB`);
+		console.log(`RAM free: ${freeMem.toFixed(2)} GB`);
+	}
 	httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 	await startKafkaConsumer();
 };
